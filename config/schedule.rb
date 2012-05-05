01106ef10c1a3e92bced47bfc08033b6bf292b41
ian gallina <ian.gallina@gmail.com>
2012-05-05 12:21:20 -0300

    coloquei o schedule rb ai pra testar

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,16 +5,16 @@
 
 # Example:
 #
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+set :output, "/path/to/my/cron_log.log"
+
+every 2.hours do
+  command "/usr/bin/some_great_command"
+  runner "MyModel.some_method"
+  rake "some:great:rake:task"
+end
+
+every 4.days do
+  runner "AnotherModel.prune_old_records"
+end
 
 # Learn more: http://github.com/javan/whenever
