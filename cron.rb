aa29cae1f0c8b2ac442439d6be2262598ccc2294
Célio Fonseca <celio.la@gmail.com>
2012-05-08 16:54:41 -0300

    Execucao das rules, validando se ela existe. Rule de preco criada.

--- cron.rb
+++ cron.rb
@@ -2,50 +2,86 @@
 require 'yaml'
 require_relative 'db'
 
-# instancia o DBUtil
-db_util = DBUtil.new
-
-# le da yml quais sites a serem peneirados
-sites = YAML::load_file('sites.yml')
-
-sites.each do |site|
-  base_url = site[1]['base_url']
-  regra_preco = site[1]['regra_preco']
-  regra_estoque = site[1]['regra_estoque']
+class Cron
   
-  produtos = site[1]['produtos']
-  produtos.each do |produto|
-    key = produto[0]
-    produto_url = produto[1]['url']
-    nome = produto[1]['nome']
-    rules = produto[1]['rules']
+  def initialize
+    # instancia o DBUtil
+    @db_util = DBUtil.new
+  end
+  
+  def executar!
+    # le da yml quais sites a serem peneirados
+    @sites = YAML::load_file('sites.yml')
     
-    puts "Acessando: #{base_url}#{produto_url}"
-    
-    #Acessar banco para recuperar dados para comparação
-    db_produtos = db_util.get_produtos key, produto_url
-    
-    unless db_produtos.nil?
-      db_produtos.each do |db_produto|
-        puts "#{db_produto}"
-      end
-      
-      puts "Aplicando rules: "
-      rules.each do |rule|
-        puts "#{rule[1]}"
+    @sites.each do |site|
+      base_url = site[1]['base_url']
+      regra_preco = site[1]['regra_preco']
+      regra_estoque = site[1]['regra_estoque']
+
+      produtos = site[1]['produtos']
+      produtos.each do |produto|
+        key = produto[0]
+        produto_url = produto[1]['url']
+        nome = produto[1]['nome']
+        rules = produto[1]['rules']
+
+        # acessa o site e recupera os dados
+        puts "Acessando: #{base_url}#{produto_url}"
+        produto_atual = {:preco => 110.0}
+
+        #Acessar banco para recuperar dados para comparação
+        db_produtos = @db_util.get_produtos key, produto_url
+
+        unless db_produtos.nil?
+          puts "Aplicando rules: "
+          rules.each do |rule|
+            metodo = rule[1].split("_")[0]
+            argumento = rule[1].split("_")[1]
+
+            if self.respond_to? metodo
+              puts "Executando metodo #{metodo} com argumento #{argumento}"
+              
+              # se a verificacao passou
+              if send(metodo, db_produtos, produto_atual, argumento)
+                #chamar o notifier.rb para informar que encontrou algo
+              end
+              
+            else
+              #chamar o notifier.rb para falar que o yaml está errado
+              raise "[ERRO] Metodo :#{metodo} nao existe!"
+            end
+          end
+        end
+
+        # adiciona no banco o novo produto
+        preco = 100.0
+        estoque = true
+        @db_util.add_produto key, produto_url, preco, estoque
+
       end
     end
-    
-    # acessa o site e recupera os dados
-    
-    
-    # adiciona no banco o novo produto
-    preco = 100.0
-    estoque = true
-    db_util.add_produto key, produto_url, preco, estoque
-    
   end
+  
+  def preco produtos, produto_atual, percento
+    percento = percento.sub "%", ""
+    preco_atual = produto_atual[:preco]
+    media = 0
+    
+    produtos.each do |produto|
+      media += produto[:preco]
+    end
+    media /= produtos.count
+    
+    percento = percento.to_f / 100
+    limite = media * percento
+    
+    preco_atual >= (media + limite) || preco_atual <= (media - limite)
+  end
+  
 end
+
+cron = Cron.new
+cron.executar!
 
 
 # aguarda retorno para cada um dos sites, caso contrário agenda uma chamada em breve
